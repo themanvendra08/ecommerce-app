2ddd4f9d80a9e82d4152a57f39085b34a7244bf2
themanvendra08 <manvendra.singh@zysk.com>
2025-10-04 10:40:39 +0530

    feat: added inngest function for user management

---
+++ config/db.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const options = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose
+      .connect(`${process.env.MONGODB_URI}`, options)
+      .then(
+        (mongoose = () => {
+          return mongoose;
+        })
+      );
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectDB;
