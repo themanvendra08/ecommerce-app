74d36731c4d117c22dd5e59b17785effcbb7c2d1
themanvendra08 <manvendra.singh@zysk.com>
2025-10-07 16:56:12 +0530

    fix: build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'raw.githubusercontent.com',
-                pathname: '**',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '**',
+      },
+    ],
+  },
+  webpack(config, { dev }) {
+    if (!dev) {
+      if (!config.optimization) config.optimization = {};
+      config.optimization.minimize = true;
+      config.optimization.minimizer = [];
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
